159152a9674dbb0e4ec60d23db1f0c668c0e4ded
MKS-01 <manish10mks@gmail.com>
2020-05-03 16:24:22 +0530

    dynamicUI

--- src/scenes/productList/index.js
+++ src/scenes/productList/index.js
@@ -2,22 +2,22 @@ import React from 'react';
 import {FlatList} from 'react-native';
 import {RootSafeAreaView, RootView, RootScrollView} from '_styles/RootView';
 import ProductCard from '_components/product-card';
-import {shoesData} from './data';
+import {shoesData, denimData} from './data';
 
-const DATA = [
-  {
-    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-    title: 'First Item',
-  },
-  {
-    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-    title: 'Second Item',
-  },
-  {
-    id: '58694a0f-3da1-471f-bd96-145571e29d72',
-    title: 'Third Item',
-  },
-];
+// const DATA = [
+//   {
+//     id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+//     title: 'First Item',
+//   },
+//   {
+//     id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+//     title: 'Second Item',
+//   },
+//   {
+//     id: '58694a0f-3da1-471f-bd96-145571e29d72',
+//     title: 'Third Item',
+//   },
+// ];
 
 const ProductList = ({route}) => {
   const {type} = route.params;
@@ -36,8 +36,8 @@ const List = (props) => {
 
   return (
     <FlatList
-      // data={type === 'shoes' ? shoesData : DATA}
-      data={shoesData}
+      data={type === 'shoes' ? shoesData : denimData}
+      // data={shoesData}
       renderItem={(data) => <ProductCard data={data} />}
       keyExtractor={(data) => data.id}
       showsVerticalScrollIndicator={false}
