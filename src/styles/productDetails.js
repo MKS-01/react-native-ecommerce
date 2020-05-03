2e69ac8216a2e9777c9ce7dba884a5519a17eb2e
MKS-01 <manish10mks@gmail.com>
2020-05-03 16:28:13 +0530

    commented data removed

--- src/styles/productDetails.js
+++ src/styles/productDetails.js
@@ -1,13 +1,11 @@
 import styled from 'styled-components/native';
 import {scaleSize, scaleFont} from './mixins';
-import {FONT_SIZE_16, FONT_SIZE_12} from './typography';
 
 export const Card = styled.TouchableOpacity`
   padding: ${scaleSize(20)}px;
   border: 1px solid #ede8df;
   border-radius: ${scaleSize(3)}px;
   margin-bottom: 10px;
-  /* background-color: red; */
 `;
 
 export const ImageView = styled.Image`
